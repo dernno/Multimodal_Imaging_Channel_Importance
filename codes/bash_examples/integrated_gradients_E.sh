696356f984a4a61e308099df83d8b02a2b183cdd
dennor <node8232@student.uu.se>
2025-06-27 20:00:25 +0200

    IG_bash

--- codes/bash_examples/integrated_gradients_E.sh
+++ codes/bash_examples/integrated_gradients_E.sh
@@ -10,7 +10,7 @@ module load Python/3.11.5-GCCcore-13.2.0
 # Aktiviere die virtuelle Umgebung
 source /mimer/NOBACKUP/groups/snic2021-7-128/nora/nora_env/bin/activate  # Pfad zu deiner venv
 
-python /mimer/NOBACKUP/groups/snic2021-7-128/nora/letitshine/codes/integrated_gradients_FLEXIBLE.py \
+python /mimer/NOBACKUP/groups/snic2021-7-128/nora/letitshine/codes/integrated_gradients.py \
 	--path_to_folds '/mimer/NOBACKUP/groups/snic2021-7-128/nora/letitshine/folds/FINAL_FOLDS/p123_20' \
 	--path_to_images '/mimer/NOBACKUP/groups/snic2021-7-128/nora/data'\
 	--mode MM6 --fusion_mode E --channel 6 --mixup --split_mode "train_test" --pretrained True \
